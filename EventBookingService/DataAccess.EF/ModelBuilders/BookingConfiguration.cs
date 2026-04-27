e58bc34b28760d159424825672c40c3d1702bdc6
Andrey Mordvinov <mordvinov14101998@gmail.com>
2026-04-27 22:49:31 +0300

    Билдеры для моделей для Ef

---
+++ EventBookingService/DataAccess.EF/ModelBuilders/BookingConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Bookings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.EF.ModelBuilders;
+
+public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder
+            .ToTable(nameof(Booking))
+            .HasKey(b => b.Id);
+
+        builder.Property(b => b.Status)
+            .IsRequired();
+
+        builder.Property(b => b.CreatedAt)
+            .IsRequired()
+            .HasColumnType("timestamp with time zone")
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+        builder.Property(b => b.ProcessedAt)
+            .HasColumnType("timestamp with time zone");
+
+        builder
+            .HasOne(b => b.Event)
+            .WithMany(e => e.Bookings)
+            .HasForeignKey(b => b.EventId);
+    }
+}
