a6f22e080ccc0afe1bda3cfa52f43a03cf4088eb
Andrey Mordvinov <mordvinov14101998@gmail.com>
2026-05-27 21:48:29 +0300

    Предварительно разбиение на 4 проекта

--- EventBookingService/Shared/Locking/ISemaphoreGetter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Locking;
-
-/// <summary>
-/// Интерфейс для получения семафора блокировки
-/// </summary>
-public interface ISemaphoreGetter
-{
-    SemaphoreSlim SemaphoreSlim { get; }
-}
